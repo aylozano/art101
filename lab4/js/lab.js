b687126a82fa594d84da6d49b87f01cda3d7380e
aylozano <aylozano@ucsc.edu>
2022-10-15 12:51:31 -0700

    lab4
    This is my submission for LAB 4, we analyze how to start coding. My idea was how to make someone smile, very broad topic, and I am not sure how it works but I need to get some points.

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+//Approach someone
+  //Say Hi
+    //Wait for response
+      //If friendly response is obtain
+    //Insert “dumb joke”
+//Disappear.
