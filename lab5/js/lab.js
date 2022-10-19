9328dbce913b787ef8d97f079cbedf849e4ed5c5
aylozano <aylozano@ucsc.edu>
2022-10-19 10:49:59 -0700

    LAB5
    Intro JavaScript.
    New html:
    I learn:
    How to fix Alex image.
    Upload a picture for background.
    JS

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+
+
+/**
+ * Author:    Andrik Lozano
+ * Created:   10/18/2022
+ *
+ * (c) Copyright by potatoe corp.
+ **/
+
+ // Define Variables
+ make = "chrysler"
+ model = "crossfire"
+ color = "white"
+ year = 2004
+
+ // Calculate
+ age = ( 2022 - year )
+
+ // Output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " year<br>");
