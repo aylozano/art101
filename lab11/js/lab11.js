a123263185b900c3c5af52aca6fb91c2e76e13b7
aylozano <aylozano@ucsc.edu>
2022-11-08 07:05:21 -0800

    lab11
    intro jquery, we created buttons and add event listeners on JS

---
+++ lab11/js/lab11.js
@@ -0,0 +1,23 @@
+
+ // * Author:    Andrik Lozano
+ // * Created:   10/31/2022
+ // * (c) Copyright by potatoe corp.
+
+$('#challenge').append('<button id="buttonChalEl"> click me daddy. </button>')
+
+$('#problem').append('<button id="buttonProbEl"> No, allow me sir being click by you, distingishble gentlement. </button>')
+
+$('#result').append('<button id="buttonResEl"> NOOOO!! click ME daddy. </button>')
+
+
+$('#buttonChalEl').click( function(){
+  $('#challenge').toggleClass("special");
+});
+
+$('#buttonResEl').click( function(){
+  $('#result').toggleClass("double-special");
+});
+
+$('#buttonProbEl').click( function(){
+  $('#problem').toggleClass("triplediple-special");
+});
